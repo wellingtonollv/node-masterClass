9a949ab949e1420810b121f8a78f7bf52b331335
Wellington Oliveira <28570773+wellingtonollv@users.noreply.github.com>
2021-10-03 19:46:13 -0300

    end of http module

--- NodeHTTP/server.js
+++ NodeHTTP/server.js
@@ -7,12 +7,47 @@ const todos = [
 ]
 
 const server = http.createServer((req, res)=>{
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('X-Powered-By', 'miltom');
-    res.end(JSON.stringify({
-        sucess: true,
-        data: todos
-    }));
+    const {method, url} = req
+   let body = [];
+   
+   req
+    .on('data', chunk => {
+        body.push(chunk);
+    })
+    .on('end', () => {
+        body= Buffer.concat(body).toString();   
+
+        let status = 404;
+        const response = {
+            sucess:false,
+            data:null
+        }   
+
+        if(method==='GET' && url==='/todos'){
+            status = 200;
+            response.sucess = true;
+            response.data= todos
+        } else if(method === 'POST' && url==='/todos') { 
+            const {id, text} = JSON.parse(body);
+
+            if(!id || !text){
+                status=400;
+                response.error= 'please insert missing id or text'
+            }else{
+                todos.push({id, text});
+                status=201;
+                response.sucess=true;
+                response.data=todos;
+            }
+        }
+
+        res.writeHead(status,response);
+
+        res.end(
+            JSON.stringify(response)
+        )
+    })
+    
 });
 
 const PORT = 5000;
